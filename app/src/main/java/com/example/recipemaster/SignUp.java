f7682abbd9dda05e9f2e3fbd10980182be2b348f
ainwg <87463461+ainwg@users.noreply.github.com>
2023-10-22 21:43:22 +0800

    Create SignUp and Login

---
+++ app/src/main/java/com/example/recipemaster/SignUp.java
@@ -0,0 +1,29 @@
+package com.example.recipemaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUp extends AppCompatActivity {
+
+    Button buttonSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+
+        buttonSignUp = findViewById(R.id.btnSignUp);
+
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUp.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
