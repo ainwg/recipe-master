fe1e771b4e694ee804228924224d26a3874a5825
ainwg <87463461+ainwg@users.noreply.github.com>
2023-10-23 23:14:54 +0800

    Update SignUp and Login

--- app/src/main/java/com/example/recipemaster/SignUp.java
+++ app/src/main/java/com/example/recipemaster/SignUp.java
@@ -6,18 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
 
-    Button buttonSignUp;
+    TextView textLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
 
-        buttonSignUp = findViewById(R.id.btnSignUp);
+        textLogin = findViewById(R.id.tvLogin);
 
-        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+        textLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SignUp.this, Login.class);
