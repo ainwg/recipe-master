317229f7249f9b42839f4b2503f78d8e8b3f3393
ainwg <87463461+ainwg@users.noreply.github.com>
2023-10-25 20:33:35 +0800

    Update Upload activity

--- app/src/main/java/com/example/recipemaster/Homepage.java
+++ app/src/main/java/com/example/recipemaster/Homepage.java
@@ -2,13 +2,28 @@ package com.example.recipemaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Homepage extends AppCompatActivity {
 
+    FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
+
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Homepage.this, Upload.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/recipemaster/Upload.java
+++ app/src/main/java/com/example/recipemaster/Upload.java
@@ -2,13 +2,29 @@ package com.example.recipemaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class Upload extends AppCompatActivity {
+
+    ImageView uploadImage;
+    EditText uploadTopic, uploadDesc, uploadLang;
+    Button saveButton;
+    String imageURL;
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.upload);
+
+        uploadImage = findViewById(R.id.uploadImg);
+        uploadDesc = findViewById(R.id.uploadDesc);
+        uploadTopic = findViewById(R.id.uploadTopic);
+        uploadLang = findViewById(R.id.uploadLang);
+        saveButton = findViewById(R.id.saveButton);
     }
 }
