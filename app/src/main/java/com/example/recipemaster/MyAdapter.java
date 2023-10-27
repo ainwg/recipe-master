bc0d3f77300d7d10ae507a021fb12c2846c25677
ainwg <87463461+ainwg@users.noreply.github.com>
2023-10-27 21:39:12 +0800

    Update MyAdapter.java

--- app/src/main/java/com/example/recipemaster/MyAdapter.java
+++ app/src/main/java/com/example/recipemaster/MyAdapter.java
@@ -1,4 +1,59 @@
 package com.example.recipemaster;
 
-public class MyAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+    private Context context;
+    private List<DataClass> dataList;
+
+    public MyAdapter(Context context, List<DataClass> dataList) {
+        this.context = context;
+        this.dataList = dataList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
+
+class MyViewHolder extends RecyclerView.ViewHolder {
+
+    ImageView recImage;
+    TextView recTitle, recDesc, recLang;
+    CardView recCard;
+
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        recImage = itemView.findViewById(R.id.recImage);
+        recTitle = itemView.findViewById(R.id.recTitle);
+        recDesc = itemView.findViewById(R.id.recDesc);
+        recLang = itemView.findViewById(R.id.recLang);
+        recCard = itemView.findViewById(R.id.rv);
+
+
+    }
 }
