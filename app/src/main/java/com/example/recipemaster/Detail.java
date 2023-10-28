29cb49bf4b9a54cb07939086d4ceeff7974501b6
ainwg <87463461+ainwg@users.noreply.github.com>
2023-10-28 19:59:15 +0800

    Update Detail.java

--- app/src/main/java/com/example/recipemaster/Detail.java
+++ app/src/main/java/com/example/recipemaster/Detail.java
@@ -3,12 +3,29 @@ package com.example.recipemaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class Detail extends AppCompatActivity {
 
+    TextView detailTitle, detailDesc;
+    ImageView detailImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail);
+
+        detailTitle = findViewById(R.id.detailTitle);
+        detailDesc = findViewById(R.id.detailDesc);
+        detailImage = findViewById(R.id.detailImage);
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null) {
+            detailDesc.setText(bundle.getString("Description");
+            detailTitle.setText(bundle.getString("Title"));
+            Glide.with(this).load(bundle.getString("Image")).into(detailImage);
+        }
     }
 }
